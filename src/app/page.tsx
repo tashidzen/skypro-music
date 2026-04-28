2ac7150aff1c77f5d34524135cdaf6eafeb15183
tashidzen <tashidzen@yandex.ru>
2026-04-28 23:45:22 +0300

    Выделяет боковую панель в отдельный компонент.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import './page.css';
-import Image from 'next/image';
-import Link from 'next/link';
 import styles from './page.module.css';
 import Bar from '@/components/Bar/Bar';
 import MainNav from '@/components/MainNavigation/mainNavigation';
 import CenterBlock from '@/components/CenterBlock/centerBlock';
+import MainSidebar from '@/components/MainSidebar/mainSidebar';
 
 export default function Home() {
   return (
@@ -13,56 +11,10 @@ export default function Home() {
         <main className={styles.main}>
           <MainNav />
           <CenterBlock />
-          <div className={'main__sidebar'}>
-            <div className={'sidebar__personal'}>
-              <p className={'sidebar__personalName'}>Sergey.Ivanov</p>
-              <div className={'sidebar__icon'}>
-                <svg>
-                  <use xlinkHref="/img/icon/sprite.svg#logout"></use>
-                </svg>
-              </div>
-            </div>
-            <div className={'sidebar__block'}>
-              <div className={'sidebar__list'}>
-                <div className={'sidebar__item'}>
-                  <Link className={'sidebar__link'} href="#">
-                    <Image
-                      className={'sidebar__img'}
-                      src="/img/playlist01.png"
-                      alt="day's playlist"
-                      width={250}
-                      height={170}
-                    />
-                  </Link>
-                </div>
-                <div className={'sidebar__item'}>
-                  <Link className={'sidebar__link'} href="#">
-                    <Image
-                      className={'sidebar__img'}
-                      src="/img/playlist02.png"
-                      alt="day's playlist"
-                      width={250}
-                      height={170}
-                    />
-                  </Link>
-                </div>
-                <div className={'sidebar__item'}>
-                  <Link className={'sidebar__link'} href="#">
-                    <Image
-                      className={'sidebar__img'}
-                      src="/img/playlist03.png"
-                      alt="day's playlist"
-                      width={250}
-                      height={170}
-                    />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+          <MainSidebar />
         </main>
         <Bar />
-        <footer className="footer"></footer>
+        <footer className={styles.footer}></footer>
       </div>
     </div>
   );
---
+++ src/components/MainSidebar/mainSidebar.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './mainSidebar.module.css';
+
+export default function MainSidebar() {
+  return (
+    <div className={styles.main__sidebar}>
+      <div className={styles.sidebar__personal}>
+        <p className={styles.sidebar__personalName}>Sergey.Ivanov</p>
+        <div className={styles.sidebar__icon}>
+          <svg>
+            <use xlinkHref="/img/icon/sprite.svg#logout"></use>
+          </svg>
+        </div>
+      </div>
+      <div className={styles.sidebar__block}>
+        <div className={styles.sidebar__list}>
+          <div className={styles.sidebar__item}>
+            <Link className={styles.sidebar__link} href="#">
+              <Image
+                className={styles.sidebar__img}
+                src="/img/playlist01.png"
+                alt="day's playlist"
+                width={250}
+                height={170}
+              />
+            </Link>
+          </div>
+          <div className={styles.sidebar__item}>
+            <Link className={styles.sidebar__link} href="#">
+              <Image
+                className={styles.sidebar__img}
+                src="/img/playlist02.png"
+                alt="day's playlist"
+                width={250}
+                height={170}
+              />
+            </Link>
+          </div>
+          <div className={styles.sidebar__item}>
+            <Link className={styles.sidebar__link} href="#">
+              <Image
+                className={styles.sidebar__img}
+                src="/img/playlist03.png"
+                alt="day's playlist"
+                width={250}
+                height={170}
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
